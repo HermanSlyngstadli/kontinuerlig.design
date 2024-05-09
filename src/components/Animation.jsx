df023bf7acddc6886dd293b9854964431e0eab51
gregergahre <greger.gahre@bekk.no>
2024-05-09 16:57:52 +0200

    animation fixed

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import lineAnimation from '../images/animation.gif'
 
-// const LineAnimation = styled.img`
-//     width: 100%;
-//     @media screen and (max-width: 550px) {
-//         display: none;
-//     }
-// `
+const LineAnimation = styled.img`
+    width: 100%;
+    @media screen and (max-width: 550px) {
+        display: none;
+    }
+`
 
-// function Animation() {
-//     return (
-//         <div>
-//             <LineAnimation src="../src/images/animation.gif" alt="Your GIF" />
-//         </div>
-//     )
-// }
+function Animation() {
+    return (
+        <div>
+            <LineAnimation src={lineAnimation} alt="Your GIF" />
+        </div>
+    )
+}
 
-// export default Animation
+export default Animation
